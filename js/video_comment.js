5861d9e36370d6272a61502b68e29db9f86b6751
kimkyounghee <kh5742@naver.com>
2023-08-04 17:57:54 +0900

    video_comment.js 수정 by.김경희

--- js/video_comment.js
+++ js/video_comment.js
@@ -1,81 +1,53 @@
-const commentForm = document.getElementById('comment-form');
-const commentInput = document.getElementById('comment-input');
-const commentsList = document.getElementById('comments-list');
+const input = document.querySelector('.Comment-input');
+const itemAdd = document.querySelector('.Comment-Add');
+const comments = document.querySelector('.Comments');
 
-// 로컬 스토리지에 저장된 댓글을 가져와서 초기화
-let comments = JSON.parse(localStorage.getItem('comments')) || [];
-
-// 댓글 추가 함수
-function addComment() {
-    const commentText = commentInput.value;
-    if (!commentText) {
-        alert('Please enter a comment.');
+const onAdd = () => {
+    const text = input.value;
+    if (input.value === '') {
+        input.focus();
         return;
     }
 
-    const newComment = {
-        id: Date.now(), // 댓글 고유 ID (실제로는 데이터베이스에서 생성)
-        text: commentText,
-    };
+    const item = document.createElement('li');
+    item.setAttribute('class', 'item');
 
-    comments.push(newComment);
-    saveComments();
-    renderComments();
+    const itemText = document.createElement('span');
+    itemText.setAttribute('class', 'itemText');
+    itemText.innerHTML = text;
 
-    commentInput.value = ''; // 입력 필드 초기화
-}
-
-// 댓글 수정 함수
-function editComment(id) {
-    const editedText = prompt('Edit your comment:', comments.find(comment => comment.id === id).text);
-    if (editedText === null) {
-        return; // 사용자가 수정을 취소한 경우
-    }
-
-    comments = comments.map(comment => {
-        if (comment.id === id) {
-            return { ...comment, text: editedText };
+    const itemEdit = document.createElement('button'); // 수정 버튼 생성
+    itemEdit.setAttribute('class', 'itemEdit'); // 클래스 추가
+    itemEdit.innerHTML = '수정'; // 버튼 내용 설정
+    itemEdit.addEventListener('click', () => {
+        const editText = prompt('수정할 내용을 입력하세요:', itemText.innerHTML);
+        if (editText !== null) {
+            itemText.innerHTML = editText;
         }
-        return comment;
     });
 
-    saveComments();
-    renderComments();
-}
-
-// 댓글 삭제 함수
-function deleteComment(id) {
-    comments = comments.filter(comment => comment.id !== id);
-    saveComments();
-    renderComments();
-}
-
-// 댓글 목록을 화면에 그려주는 함수
-function renderComments() {
-    commentsList.innerHTML = '';
-    comments.forEach(comment => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <span>${comment.text}</span>
-            <button onclick="editComment(${comment.id})">Edit</button>
-            <button onclick="deleteComment(${comment.id})">Delete</button>
-        `;
-        commentsList.appendChild(li);
+    const itemDel = document.createElement('button');
+    itemDel.setAttribute('class', 'itemDel');
+    itemDel.innerHTML = '삭제';
+    itemDel.addEventListener('click', () => {
+        comments.removeChild(item);
     });
-}
 
-// 로컬 스토리지에 댓글 저장 함수
-function saveComments() {
-    localStorage.setItem('comments', JSON.stringify(comments));
-}
+    item.appendChild(itemText);
+    item.appendChild(itemEdit); // 수정 버튼을 아이템에 추가
+    item.appendChild(itemDel);
+    comments.appendChild(item);
+    input.value = '';
+    input.focus();
+};
 
-// 엔터 키 이벤트 처리 함수
-function handleKeyPress(event) {
+itemAdd.addEventListener('click', () => {
+    onAdd();
+});
+
+itemAdd.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-        event.preventDefault();
-        addComment();
+        onAdd();
     }
-}
-
-// 페이지 로드 시 댓글 목록 렌더링
-renderComments();
+    return;
+});
